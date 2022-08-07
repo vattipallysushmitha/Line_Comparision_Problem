be0c8b212db06b8afc4457a90d9a1e0822567355
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-08-07 16:38:07 +0530

    refatored[sushmithe] modified the UC2 code

--- src/com/bridzelab/LineComparisionProblem/LineComparisionProblem.java
+++ src/com/bridzelab/LineComparisionProblem/LineComparisionProblem.java
@@ -2,44 +2,49 @@ package com.bridzelab.LineComparisionProblem;
 import java.util.Scanner;
 
 public class LineComparisionProblem {
+	//get variables
+	int x1,x2,y1,y2;
+   //Scanner Object for User Input
+    Scanner sc=new Scanner(System.in);
+	
+	//method to get input from user
+	public void getCooridinates()
+	{
+		System.out.println("Welcome to Line Comparison Program in java");
 
-	   public static void main(String[] args) {
+		System.out.println("enter x1 point");
+		x1=sc.nextInt();
+		System.out.println("enter y1 point");
+		y1=sc.nextInt();
+		System.out.println("enter x2 point");
+		x2=sc.nextInt();
+		System.out.println("enter y2 point");
+		y2=sc.nextInt();
+	}
+	//method for calculating length of line
+	public double getLengthOfLine()
+    {
+      double lengthOfLine = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+        return lengthOfLine;
+    }
+	public static void main(String[] args) {
+		LineComparisionProblem LC1 = new LineComparisionProblem();
+		LineComparisionProblem LC2= new LineComparisionProblem();
+		//geting input from the user
+	        LC1.getCooridinates();
+	        LC2.getCooridinates();
+	        
+	        double line1Length = LC1.getLengthOfLine();
+	        double line2Length = LC2.getLengthOfLine();
+	      //Displaying the Length of Lines
+	        System.out.println("Length of Line1 : "+line1Length);
+	        System.out.println("Length of Line2 : "+line2Length);
 
-	      System.out.println("Welcome to Line Comparison Program on Master Branch");
-
-	      int x1,x2,y1,y2;
-	      int a1,a2,b1,b2;
-	      double distance1, distance2;
-	      Scanner scan = new Scanner(System.in);
-
-	      System.out.println("enter x1 point");
-	      x1=scan.nextInt();
-	      System.out.println("enter y1 point");
-	      y1=scan.nextInt();
-	      System.out.println("enter x2 point");
-	      x2=scan.nextInt();
-	      System.out.println("enter y2 point");
-	      y2=scan.nextInt();
-	      distance1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-	      System.out.println("distance between" + "("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+distance1);
-
-
-	      System.out.println("enter a1 point");
-	      a1=scan.nextInt();
-	      System.out.println("enter b1 point");
-	      b1=scan.nextInt();
-	      System.out.println("enter a2 point");
-	      a2=scan.nextInt();
-	      System.out.println("enter b2 point");
-	      b2=scan.nextInt();
-	      distance2 = Math.sqrt((a2-a1)*(a2-a1) + (b2-b1)*(b2-b1));
-	      System.out.println("distance between" + "("+a1+","+b1+"),"+"("+a2+","+b2+")===>"+distance2);
-
-
-	      String s1, s2;
-	      s1 = Double.toString(distance1);
-	      s2 = Double.toString(distance2);
-	      System.out.println(distance1 = (distance2));
-
-	   }
+	      //Checking the Equality of Lines
+	        if(line1Length == line2Length) {
+	            System.out.println("Length of Line1 and Line2 are Equal");
+	        } else {
+	            System.out.println("Length of Line1 and Line2 are Not Equal");
+	        }
+	    }
 	}
