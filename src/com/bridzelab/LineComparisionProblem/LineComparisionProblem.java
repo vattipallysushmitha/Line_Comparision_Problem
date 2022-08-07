eb9422e6f9f307f51cfadd3a4c2437dabe2cb8ee
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-08-07 16:25:24 +0530

    refactored [sushmitha] modified the UC1 code

--- src/com/bridzelab/LineComparisionProblem/LineComparisionProblem.java
+++ src/com/bridzelab/LineComparisionProblem/LineComparisionProblem.java
@@ -2,22 +2,35 @@ package com.bridzelab.LineComparisionProblem;
 import java.util.Scanner;
 
 public class LineComparisionProblem {
-
-	public static void main(String[] args) {
+	//get variables
+	int x1,x2,y1,y2;
+   //Scanner Object for User Input
+    Scanner sc=new Scanner(System.in);
+	
+	//method to get input from user
+	public void getCooridinates()
+	{
 		System.out.println("Welcome to Line Comparison Program in java");
 
-	      int x1,x2,y1,y2;
-	      double distance;
-	      Scanner sc=new Scanner(System.in);
-
-	      System.out.println("enter x1,y1 point");
-	      x1=sc.nextInt();
-	      y1=sc.nextInt();
-	      System.out.println("enter x2,y2 point");
-	      x2=sc.nextInt();
-	      y2=sc.nextInt();
-	      distance = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-	      System.out.println("length of line is:"+distance);
+		System.out.println("enter x1 point");
+		x1=sc.nextInt();
+		System.out.println("enter y1 point");
+		y1=sc.nextInt();
+		System.out.println("enter x2 point");
+		x2=sc.nextInt();
+		System.out.println("enter y2 point");
+		y2=sc.nextInt();
+	}
+	//method for calculating length of line
+	public double getLengthOfLine()
+    {
+      double lengthOfLine = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+        return lengthOfLine;
+    }
+	public static void main(String[] args) {
+		LineComparisionProblem LC = new LineComparisionProblem();
+	        LC.getCooridinates();
+        System.out.println("Length of Lines : "+LC.getLengthOfLine());
 
 
 	   }
